a14c0c643b4ef403947ae0499d06637c905e09c6
kuroko000 <121641551+kuroko000@users.noreply.github.com>
2024-04-16 10:53:06 -0300

    Create jogo da velha.py

---
+++ jogo da velha.py
@@ -0,0 +1,57 @@
+def novoTabuleiro():
+    return [0,0,0,
+            0,0,0,
+            0,0,0]
+
+def imprimirTabuleiro(tabuleiro):
+    for indice, valor in enumerate(tabuleiro):
+        if valor == 0:
+            print (" ", indice + 1, sep = "", end = '')
+        elif valor == 1:
+            print (" x", end = '')
+        else:
+            print(" o", end = '')
+
+        if indice == 2 or indice == 5:
+            print ("\n---+---+---\n", end = '')
+        elif indice < 8:
+            print(" |", end='')
+    print("\n")
+
+    def recebeJogada(jogador):
+        try:
+            jogado = int(input("Digite a posição a jogar 1-9 (jogador %s):"% jogador))
+            return jogada
+        except ValueError:
+            print("entrada invalida")
+            return -1
+        
+def posicaoValida (jogada, tabuleiro):
+    if jogada < 1 or jogada >9:
+        print ("Posição invalida")
+        return False
+    if tabuleiro[jogada - 1] != 0:
+        print ("posição ocupada")
+        return False
+    return True
+
+
+def mudaJogador (jogador, jogada, tabuleiro):
+    if jogador == "x":
+        tabuleiro[jogada - 1] = 1
+        return "o"
+    
+    else:
+        tabuleiro[jogada - 1] = 2
+        return "x"
+    
+def verificaFimDeJogo(numJogadas, tabuleiro):
+    #verifica linhas
+    if tabuleiro[0] == tabuleiro[1] == tabuleiro[2]: #
+        if tabuleiro [0] == 1:
+            print ("Jogador x ganhou")
+            return 1
+        elif tabuleiro[0] == 2:
+            print ("Jogador o ganhou")
+            return
+    if tabuleiro[3] == tabuleiro [4] == tabuleiro [5]:
